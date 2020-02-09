b1394c729c05820fedb8dc8f38d7ae1ce810e6fc
BrenerQuevedo <39017457+BrenerQuevedo@users.noreply.github.com>
2020-02-08 23:12:34 -0300

    rotas e páginas

---
+++ mobile/src/routes.js
@@ -0,0 +1,14 @@
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+
+import Main from './pages/Main';
+import Profile from './pages/Profile'
+
+
+const Routes = createAppContainer(
+    createSwitchNavigator({
+        Main,
+        Profile,
+    })
+);
+
+export default Routes;
