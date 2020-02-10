1ccb3e604729fa37c78aae32e9ed35979ec843a5
BrenerQuevedo <39017457+BrenerQuevedo@users.noreply.github.com>
2020-02-09 23:02:04 -0300

    acertando geolocalização

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React , {useEffect} from 'react';
+import {StyleSheet} from 'react-native';
 import MapView from 'react-native-maps';
+import {requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location';
+
 
 function Main(){
-    return <MapView style = {{flex: 1}}/>
+    useEffect(()=> {
+        async function loadInitialPosition() {
+            //faz uma requisição de permissão ao usuário
+            const {granted} = await requestPermissionsAsync(); 
+        
+            if(granted) {
+                //obtém a localização caso o usuário permita (granted), para maior precisão o GPS deve estar ligado
+                const location = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+
+                location.
+            }
+        }
+
+        loadInitialPosition()
+    }, [])
+
+    return <MapView style = {styles.map}/>
 }
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    },
+});
+
 
 export default Main;
