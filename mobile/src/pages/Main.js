3d7c176eef8b5c0bf70a36bf3e35b031c530b341
BrenerQuevedo <39017457+BrenerQuevedo@users.noreply.github.com>
2020-02-15 20:44:32 -0300

    marker e callout no mapview

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React , {useEffect, useState} from 'react';
-import {StyleSheet} from 'react-native';
-import MapView from 'react-native-maps';
+import {StyleSheet, Image, View, Text} from 'react-native';
+import MapView, {Marker, Callout} from 'react-native-maps';
 import {requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location';
 
 
@@ -36,13 +36,48 @@ function Main(){
         return null;
     }
 
-    return <MapView style = {styles.map} initialRegion={currentRegion}/>
+    return <MapView  style = {styles.map} initialRegion={currentRegion}>
+            <Marker coordinate={{latitude:-7.2399455,longitude: -35.9043105}}>
+            <Image style={styles.avatar} source={{uri: "https://avatars1.githubusercontent.com/u/39017457?s=460&v=4"}}/>
+
+            <Callout>
+                <View style={styles.callout}>
+                    <Text style={styles.devName}>Brener Quevedo</Text>
+                    <Text style={styles.devBio}>bio de teste</Text>
+                    <Text style={styles.devTechs}>ReactJS, NODE, react native</Text>
+                </View>
+            </Callout>
+
+            </Marker>
+        </MapView>
 }
 
 const styles = StyleSheet.create({
     map: {
         flex: 1
     },
+    avatar:{
+        width: 54,
+        height: 54,
+        borderRadius:4,
+        borderWidth: 4,
+        borderColor: "#fff"
+    },
+    callout: {
+        width: 260,
+
+    },
+    devName:{
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+    devBio:{
+        color: "#666",
+        marginTop: 5,
+    },
+    devTechs:{
+        marginTop: 5
+    }
 });
 
 
